1d0dea5e4775cfa6656e561814b3969344891037
wei756 <wei756fg@gmail.com>
2024-05-28 17:27:12 +0900

    로딩 스켈레톤 추가

--- src/app/[date]/loading.tsx
+++ src/app/[date]/loading.tsx
@@ -1,7 +1,26 @@
 export default function Loading() {
   return (
-    <div className="loading">
-      <div className="loading__spinner">loading...</div>
+    <>
+      <div className="py-2 text-sm font-medium w-full text-center">아침</div>
+      <MenuCardSkeleton />
+      <div className="py-2 text-sm font-medium w-full text-center">점심</div>
+      <div className="flex flex-row overflow-x-auto gap-2">
+        <MenuCardSkeleton />
+        <MenuCardSkeleton />
+      </div>
+      <div className="py-2 text-sm font-medium w-full text-center">저녁</div>
+      <MenuCardSkeleton />
+    </>
+  );
+}
+
+function MenuCardSkeleton() {
+  return (
+    <div className="menu-card flex flex-col gap-4 p-4 bg-gray-200 rounded-2xl grow h-48">
+      <div className="flex flex-row gap-2">
+        <span className="text-sm font-medium w-max">로딩중...</span>
+      </div>
+      <ul className="flex flex-col gap-1"></ul>
     </div>
   );
 }
