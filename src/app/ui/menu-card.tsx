cf1c2ee891337f9fef229438e08900f1ef1fac87
wei756 <wei756fg@gmail.com>
2024-05-29 17:38:34 +0900

    가운데 정렬 수정

--- src/app/ui/menu-card.tsx
+++ src/app/ui/menu-card.tsx
@@ -6,8 +6,8 @@ export interface Props {
 
 export default function MenuCard({ name, time, menu }: Props) {
   return (
-    <div className="menu-card flex flex-col gap-4 p-4 bg-gray-200 rounded-2xl grow">
-      <div className="flex flex-row gap-2">
+    <div className="menu-card flex flex-col gap-4 p-4 bg-gray-200 rounded-2xl grow w-0 min-w-max">
+      <div className="flex flex-row gap-2 w-max min-w-max">
         <span className="text-sm font-medium w-max">{name}</span>
         {time.map((time, index) => (
           <span key={index} className="text-sm font-normal w-max">
@@ -15,7 +15,7 @@ export default function MenuCard({ name, time, menu }: Props) {
           </span>
         ))}
       </div>
-      <ul className="flex flex-col gap-1">
+      <ul className="flex flex-col gap-1 w-max">
         {menu.map((menu, index) => (
           <li className="text-sm font-normal" key={index}>
             {menu}
--- src/app/ui/menus.tsx
+++ src/app/ui/menus.tsx
@@ -9,13 +9,15 @@ export default async function Menus({ date }: { date: Date }) {
     return (
       <>
         <div className="py-2 text-sm font-medium w-full text-center">아침</div>
-        <MenuCard
-          name="학생식당"
-          time={haksik.menus[0].time}
-          menu={haksik.menus[0].menu}
-        />
+        <div className="flex flex-row overflow-x-auto gap-2 w-full">
+          <MenuCard
+            name="학생식당"
+            time={haksik.menus[0].time}
+            menu={haksik.menus[0].menu}
+          />
+        </div>
         <div className="py-2 text-sm font-medium w-full text-center">점심</div>
-        <div className="flex flex-row overflow-x-auto gap-2">
+        <div className="flex flex-row overflow-x-auto gap-2 w-full">
           <MenuCard
             name="학생식당"
             time={haksik.menus[1].time}
@@ -28,16 +30,20 @@ export default async function Menus({ date }: { date: Date }) {
           />
         </div>
         <div className="py-2 text-sm font-medium w-full text-center">저녁</div>
-        <MenuCard
-          name="교직원식당"
-          time={gyosik.menus[1].time}
-          menu={gyosik.menus[1].menu}
-        />
-        <MenuCard
-          name="분식당"
-          time={bunsik.menus[0].time}
-          menu={bunsik.menus[0].menu}
-        />
+        <div className="flex flex-row overflow-x-auto gap-2 w-full">
+          <MenuCard
+            name="교직원식당"
+            time={gyosik.menus[1].time}
+            menu={gyosik.menus[1].menu}
+          />
+        </div>
+        <div className="flex flex-row overflow-x-auto gap-2 w-full">
+          <MenuCard
+            name="분식당"
+            time={bunsik.menus[0].time}
+            menu={bunsik.menus[0].menu}
+          />
+        </div>
       </>
     );
   } catch (e) {
