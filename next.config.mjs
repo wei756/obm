2629cb76e303b81cba797fb6b9cafb0f65d4461b
wei756 <wei756fg@gmail.com>
2024-05-29 16:29:26 +0900

    pwa 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withPWA from 'next-pwa';
+const pwa = withPWA({
+  dest: 'public',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default pwa(nextConfig);
