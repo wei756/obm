4e9aa0545c9e14d5a46529c9366d7c1981903820
wei756 <wei756fg@gmail.com>
2024-05-29 17:32:37 +0900

    heroicon 추가

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,15 +1,25 @@
+import { ChevronRightIcon, LightBulbIcon } from '@heroicons/react/24/outline';
+import { ChevronLeftIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export default function Header({ date }: { date: Date }) {
   const yesterday = new Date(date.getTime() - 24 * 60 * 60 * 1000);
   const tomorrow = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+
   return (
     <header className="flex flex-row justify-between p-8 pb-4">
-      <h1 className="text-lg font-medium">오밥무?</h1>
-      <div className="flex flex-row gap-2">
-        <Link href={`/${yesterday.toISOString().slice(0, 10)}`}>◀</Link>
+      <h1 className="text-lg font-medium flex flex-row items-center gap-2">
+        <LightBulbIcon className="size-6" />
+        오밥무?
+      </h1>
+      <div className="flex flex-row gap-2 items-center">
+        <Link href={`/${yesterday.toISOString().slice(0, 10)}`}>
+          <ChevronLeftIcon className="size-4" />
+        </Link>
         <span>{date.toISOString().slice(0, 10)}</span>
-        <Link href={`/${tomorrow.toISOString().slice(0, 10)}`}>▶</Link>
+        <Link href={`/${tomorrow.toISOString().slice(0, 10)}`}>
+          <ChevronRightIcon className="size-4" />
+        </Link>
       </div>
     </header>
   );
